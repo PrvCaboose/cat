9636175424d48ceb3a338e74e3cace6598e3368a
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2024-02-03 22:29:00 -0500

    it should switch daily . . . should

---
+++ src/main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+let catImages = ["../cat/cat1.png","../cat/cat2.png","../cat/cat3.png","../cat/cat4.png","../cat/cat5.png","../cat/cat6.png","../cat/cat7.png","../cat/cat8.png","../cat/cat9.png","../cat/cat10.png","../cat/cat11.png","../cat/cat12.png","../cat/cat13.png","../cat/cat14.png","../cat/cat15.png","../cat/cat16.png","../cat/cat17.png"];
+let shuffledImages = catImages;
+if (localStorage.getItem("Images") === null) {
+    window.localStorage.setItem("Images", JSON.stringify(shuffledImages));
+}
+let oldDate = new Date;
+let hour = oldDate.getHours();
+let day = oldDate.getDay();
+let i = 0;
+
+
+const play = () => {
+    let audio = document.querySelector("#cat-meow");
+    audio.play();
+}
+
+const updateImage = (oldHours, day) => {
+    let newDate = new Date();
+    let newHour = newDate.getHours();
+    let newDay = newDate.getDay();
+    i = newDate.getDay();
+    shuffledImages = JSON.parse(window.localStorage.getItem("Images"));
+
+    // If the day changes, change the image
+    if (oldHours == 23 && newHour == 0) {
+        const background = document.querySelector("html").style.backgroundImage = `url(${shuffledImages[i]})`;
+        const downloadLink = document.querySelector("#download-link").setAttribute("href", shuffledImages[i]);
+    }
+
+    if (day == 6 && newDay == 0) {
+        shuffledImages = shuffle(catImages);
+        window.localStorage.setItem("Images", JSON.stringify(shuffledImages));
+    }
+
+    console.log(shuffledImages[0]);
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+
+setInterval(function () {
+    updateImage(hour, day);
+    let date = new Date();
+    day = date.getDay();
+    hour = date.getHours();
+}, 1000);
+
+window.onload = () => {
+    let date = new Date();
+    let i = date.getDay();
+    shuffledImages = JSON.parse(window.localStorage.getItem("Images"));
+    const background = document.querySelector("html").style.backgroundImage = `url(${shuffledImages[i]})`;
+    const downloadLink = document.querySelector("#download-link").setAttribute("href", shuffledImages[i]);
+}
